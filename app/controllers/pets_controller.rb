1bf6f6db6a4167ab064b82f6f287c81c104d123b
Jocelyn Yuan <jocelyn19950627@yahoo.com.tw>
2020-09-17 23:21:02 +0900

    pet controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,33 @@
+class PetsController < ApplicationController
+  def index
+    Pets.all
 
+  end
 
+  def show
+    @pet = Pet.find(params[:id])
+
+  end
+
+  def new
+    @pet = Pet.new
+    authorize @pet
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.user = current_user
+    if @pet.save
+      redirect_to pets_path
+    else
+      render :new
+    end
+    authorize @pet
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :profile, :breed, :age)
+  end
+end
