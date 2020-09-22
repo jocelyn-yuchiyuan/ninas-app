57ca412a46c1a14e2a3b4b39c7298285d5d9a40f
Jocelyn Yuan <jocelyn19950627@yahoo.com.tw>
2020-09-22 18:56:57 +0900

    new pet

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,6 +1,6 @@
 class PetsController < ApplicationController
   def index
-    Pets.all
+    @pet = policy_scope(Pet)
 
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,1 +1,28 @@
-users_controller.rb
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+    authorize @user
+  end
+
+  def create
+    @user = User.new(user_params)
+    authorize @user
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @pet = User.find(params[:id])
+    authorize current_user
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :first_name, :last_name, :password, :photo )
+  end
+end
